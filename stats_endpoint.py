857f91e6338b6eb705292fc50474c87d99d762a1
Daniel Corcoran <danielc.92@outlook.com>
2019-05-16 12:33:36 +1000

    Added and tested stats_endpoint successfully

---
+++ stats_endpoint.py
@@ -0,0 +1,24 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+sys.path.append('..')
+
+# our demo filter that filters by geometry, date and cloud cover
+from demo_filters import redding_reservoir
+
+# Stats API request object
+stats_endpoint_request = {
+  "interval": "day",
+  "item_types": ["REOrthoTile"],
+  "filter": redding_reservoir
+}
+
+# fire off the POST request
+result = \
+  requests.post(
+    'https://api.planet.com/data/v1/stats',
+    auth=HTTPBasicAuth(os.environ['PLANET_API_KEY'], ''),
+    json=stats_endpoint_request)
+
+print result.text
